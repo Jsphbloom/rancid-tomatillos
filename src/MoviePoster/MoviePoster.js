4afdadfe16edf133518fa1bec168ab2d350d7bbf
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-04-03 16:50:06 -0600

    imported up and down arrows from data icon pngs

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,12 +1,21 @@
 import './MoviePoster.css';
+import downVoteArrow from '../icons/downvote.png'
+import upVoteArrow from '../icons/upvote.png'
 
 function MoviePoster({poster_path, title, vote_count, id, upVoteMovie, downVoteMovie}) {
   return (
     <section className='MoviePoster'>
       <img src={poster_path} alt={title} />
-      <button onClick={() => upVoteMovie(id)}>Upvote</button>
+      
+      <button onClick={() => upVoteMovie(id)}>
+        <img src={upVoteArrow} alt="Downvote"/>
+      </button>
+
       <p>{vote_count}</p>
-      <button onClick={() => downVoteMovie(id)}>Down Vote</button>
+
+      <button onClick={() => downVoteMovie(id)}>
+        <img src={downVoteArrow} alt="Downvote"/>
+      </button>
     </section>
   );
 }
