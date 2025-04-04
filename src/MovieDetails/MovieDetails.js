216482f8c2e6670cf5a1a9b188eb1fac664bb52e
Joseph Bloom <jsphbloom@gmail.com>
2025-04-04 14:42:31 -0600

    home button icon added, genres added to movie details display

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,12 +1,16 @@
 import './MovieDetails.css';
+import home from '../icons/home.png'
 
 function MovieDetails({ movieDetails, goHome }) {
   return (
     <section className='MovieDetails'>
       <img src={movieDetails.backdrop_path} />
       <p>{movieDetails.title}</p>
+      <p>{movieDetails.genre_ids.join(', ')}</p>
       <p>{movieDetails.overview}</p>
-      <button onClick={goHome}>Go Home</button>
+      <button onClick={goHome}>
+        <img src={home} />
+      </button>
     </section>
   );
 }
