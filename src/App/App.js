8292430b2ea1c2cd776b4fb0e0002d556cebd07a
Joseph Bloom <jsphbloom@gmail.com>
2025-04-10 20:29:22 -0600

    added CSS styling for the movies container, movie posters and movie details. Added classNames to buttons. Added a section for movie details for styling purposes.

--- src/App/App.js
+++ src/App/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <main className='App'>
       <header>
-        <h1>rancid tomatillos</h1>
+        <h1>Rancid Tomatillos</h1>
       </header>
       
       <Routes>
